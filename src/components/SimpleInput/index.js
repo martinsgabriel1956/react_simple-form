d8585439e064693afb28d3de0c2cf1bcc3fb2888
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-06 14:25:07 -0300

    feat: adding basic validation

--- src/components/SimpleInput/index.js
+++ src/components/SimpleInput/index.js
@@ -1,10 +1,12 @@
 import { useRef, useState } from "react";
 
+import { toast, Toaster } from "react-hot-toast";
+
 import { Button } from "../UI/Button";
 
 export function SimpleInput() {
   const inputNameRef = useRef();
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
   function handleNameInputChange(e) {
     setName(e.target.value);
@@ -12,29 +14,36 @@ export function SimpleInput() {
 
   function handleFormSubmission(e) {
     e.preventDefault();
-    console.log(name);
+    
+    if (name.trim() === "") {
+      toast.error("Preencha todos os campos!");
+      return;
+    }
+
     const value = inputNameRef.current.value;
     console.log(value);
 
-    setName('');
+    setName("");
   }
 
-
   return (
-    <form onSubmit={handleFormSubmission}>
-      <div className="form-control">
-        <label htmlFor="name">Your Name</label>
-        <input 
-          ref={inputNameRef} 
-          type="text" 
-          id="name" 
-          onChange={handleNameInputChange} 
-          value={name}
-        />
-      </div>
-      <div className="form-actions">
-        <Button>Submit</Button>
-      </div>
-    </form>
+    <>
+      <Toaster />
+      <form onSubmit={handleFormSubmission}>
+        <div className="form-control">
+          <label htmlFor="name">Your Name</label>
+          <input
+            ref={inputNameRef}
+            type="text"
+            id="name"
+            onChange={handleNameInputChange}
+            value={name}
+          />
+        </div>
+        <div className="form-actions">
+          <Button>Submit</Button>
+        </div>
+      </form>
+    </>
   );
 }
